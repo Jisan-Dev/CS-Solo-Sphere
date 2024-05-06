e19a9da2b7d359fe69d8341bca24d7f219e36a5e
Jisun696 <istiakkashem35@gmail.com>
2024-05-06 21:53:50 +0600

    footer added

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import bgImg from '../assets/images/register.jpg';
+import logo from '../assets/images/logo.png';
 
 const Register = () => {
   return (
@@ -6,7 +8,7 @@ const Register = () => {
       <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg  lg:max-w-4xl ">
         <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
           <div className="flex justify-center mx-auto">
-            <img className="w-auto h-7 sm:h-8" src="https://merakiui.com/images/logo.svg" alt="" />
+            <img className="w-auto h-7 sm:h-10" src={logo} alt="" />
           </div>
 
           <p className="mt-3 text-xl text-center text-gray-600 ">Get Your Free Account Now.</p>
@@ -118,7 +120,7 @@ const Register = () => {
         <div
           className="hidden bg-cover bg-center lg:block lg:w-1/2"
           style={{
-            backgroundImage: `url('https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80')`,
+            backgroundImage: `url(${bgImg})`,
           }}></div>
       </div>
     </div>
