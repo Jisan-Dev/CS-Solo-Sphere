7c1530451205e16773f735fd4f1ff4690a900ef6
Jisun696 <istiakkashem35@gmail.com>
2024-05-06 21:05:37 +0600

    added authprovider context and firebase setup

---
+++ client/src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDjzcp_HoKM9Xz26a8G6rEVa4gWcldlDtU',
+  authDomain: 'cs-solo-sphere.firebaseapp.com',
+  projectId: 'cs-solo-sphere',
+  storageBucket: 'cs-solo-sphere.appspot.com',
+  messagingSenderId: '198696061973',
+  appId: '1:198696061973:web:9aab24e7cabc792f70121c',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
