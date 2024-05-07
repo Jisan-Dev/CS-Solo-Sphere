08558fc6c8e3c878570f83963e5a05de8fc6dea0
Jisun696 <istiakkashem35@gmail.com>
2024-05-07 15:53:53 +0600

    edited some styles

--- client/src/routes/Root.jsx
+++ client/src/routes/Root.jsx
@@ -6,7 +6,9 @@ const Root = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <div className="min-h-[calc(100vh-306px)]">
+        <Outlet />
+      </div>
       <Footer />
     </>
   );
