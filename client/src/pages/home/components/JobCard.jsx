4d87abc91d9bf2db3ccb26afb8cd8c25a61d7f8a
Jisun696 <istiakkashem35@gmail.com>
2024-05-08 00:41:43 +0600

    updated jobcard with fetched data and displayed them category wise in tabs at homepage

--- client/src/pages/home/components/JobCard.jsx
+++ client/src/pages/home/components/JobCard.jsx
@@ -1,19 +1,16 @@
 /* eslint-disable react/prop-types */
 
-const JobCard = () => {
+const JobCard = ({ job }) => {
   return (
-    <div className="w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all">
+    <div className="w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all flex flex-col">
       <div className="flex items-center justify-between">
-        <span className="text-xs font-normal text-gray-800 ">Deadline: 20/12/2024</span>
-        <span className="px-3 py-1 text-[10px] text-blue-800 uppercase bg-blue-200 rounded-full">Web Development</span>
+        <span className="text-xs font-normal text-gray-800 ">{job?.deadline}</span>
+        <span className="px-3 py-1 text-[10px] text-blue-800 uppercase bg-blue-200 rounded-full">{job?.category}</span>
       </div>
 
-      <div>
-        <h1 className="mt-2 text-lg font-semibold text-gray-800 ">Build Dynamic Website Using React</h1>
-
-        <p className="mt-2 text-sm text-gray-600 ">Lorem ipsum dolor sit adipisicing elit...</p>
-        <p className="mt-2 text-sm font-bold text-gray-600 ">Range: $100 - $150</p>
-      </div>
+      <h1 className="mt-2 text-lg font-semibold text-gray-800">{job?.job_title}</h1>
+      <p className="mt-2 text-sm text-gray-600 flex-grow">{job?.description}</p>
+      <p className="mt-2 text-sm font-bold text-gray-600 ">{`Range: $${job?.min_price} - $${job?.max_price}`}</p>
     </div>
   );
 };
--- client/src/pages/home/components/TabCategories.jsx
+++ client/src/pages/home/components/TabCategories.jsx
@@ -22,16 +22,32 @@ const TabCategories = ({ jobs }) => {
 
         <TabPanel>
           <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {jobs.map((job) => (
-              <JobCard key={job._id} job={job} />
-            ))}
+            {jobs
+              .filter((job) => job.category === 'Web Development')
+              .map((job) => (
+                <JobCard key={job._id} job={job} />
+              ))}
           </div>
         </TabPanel>
+
         <TabPanel>
-          <h2>Any content 2</h2>
+          <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {jobs
+              .filter((job) => job.category === 'Graphic Design')
+              .map((job) => (
+                <JobCard key={job._id} job={job} />
+              ))}
+          </div>
         </TabPanel>
+
         <TabPanel>
-          <h2>Any content 3</h2>
+          <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {jobs
+              .filter((job) => job.category === 'Digital Marketing')
+              .map((job) => (
+                <JobCard key={job._id} job={job} />
+              ))}
+          </div>
         </TabPanel>
       </Tabs>
     </div>
