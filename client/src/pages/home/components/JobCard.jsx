1bc1a549c235664d988ca372144f9bde59d3d04d
Jisun696 <istiakkashem35@gmail.com>
2024-05-08 01:35:03 +0600

    made the description shorter and used title attribute

--- client/src/pages/home/components/JobCard.jsx
+++ client/src/pages/home/components/JobCard.jsx
@@ -7,12 +7,14 @@ const JobCard = ({ job }) => {
   return (
     <Link to={`/jobs/${_id}`} className="w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all flex flex-col">
       <div className="flex items-center justify-between">
-        <span className="text-xs font-normal text-gray-800 ">{deadline}</span>
+        <span className="text-xs font-normal text-gray-800 ">Deadline: {deadline}</span>
         <span className="px-3 py-1 text-[10px] text-blue-800 uppercase bg-blue-200 rounded-full">{category}</span>
       </div>
 
       <h1 className="mt-2 text-lg font-semibold text-gray-800">{job_title}</h1>
-      <p className="mt-2 text-sm text-gray-600 flex-grow">{description}</p>
+      <p title={description} className="mt-2 text-sm text-gray-600 flex-grow">
+        {description.substring(0, 70)}...
+      </p>
       <p className="mt-2 text-sm font-bold text-gray-600 ">{`Range: $${min_price} - $${max_price}`}</p>
     </Link>
   );
