22a35db9a75a6b42abe099134f3d3c5f1d651880
Jisun696 <istiakkashem35@gmail.com>
2024-05-08 03:10:22 +0600

    primary test for bid form submission

--- client/src/pages/home/components/JobCard.jsx
+++ client/src/pages/home/components/JobCard.jsx
@@ -7,7 +7,7 @@ const JobCard = ({ job }) => {
   return (
     <Link to={`/jobs/${_id}`} className="w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all flex flex-col">
       <div className="flex items-center justify-between">
-        <span className="text-xs font-normal text-gray-800 ">Deadline: {deadline}</span>
+        <span className="text-xs font-normal text-gray-800 ">Deadline: {new Date(deadline).toLocaleDateString('en-GB')}</span>
         <span className="px-3 py-1 text-[10px] text-blue-800 uppercase bg-blue-200 rounded-full">{category}</span>
       </div>
 
--- client/src/pages/job-details/index.jsx
+++ client/src/pages/job-details/index.jsx
@@ -1,17 +1,38 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { AuthContext } from '../../provider/AuthProvider';
+import toast from 'react-hot-toast';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const JobDetails = () => {
+  const [startDate, setStartDate] = useState(new Date());
   const { user } = useContext(AuthContext);
   const job = useLoaderData();
-  const { _id, job_title, description, deadline, category, min_price, max_price } = job || {};
+  const { _id, job_title, description, deadline, category, min_price, max_price, buyer_email } = job || {};
+
+  const handleFormSubmission = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const jobId = _id;
+    const price = parseFloat(form.price.value);
+    if (price < parseFloat(min_price)) return toast.error('Offer more or at least equal to Minimum Price.');
+    if (price > parseFloat(max_price)) return toast.error('Offer less or at least equal to Maximum Price.');
+    const email = form.email.value;
+    const comment = form.comment.value;
+    const deadline = startDate;
+    const status = 'Pending';
+
+    const bidData = { jobId, price, email, deadline, comment, status, job_title, category, buyer_email };
+    console.table(bidData);
+  };
+
   return (
     <div className="flex flex-col md:flex-row justify-around gap-5  items-center min-h-[calc(100vh-306px)] md:max-w-screen-xl mx-auto ">
       {/* Job Details */}
       <div className="flex-1  px-4 py-7 bg-white rounded-md shadow-md md:min-h-[350px]">
         <div className="flex items-center justify-between">
-          <span className="text-sm font-light text-gray-800 ">Deadline: {deadline}</span>
+          <span className="text-sm font-light text-gray-800 ">Deadline: {new Date(deadline).toLocaleDateString('en-GB')}</span>
           <span className="px-4 py-1 text-xs text-blue-800 uppercase bg-blue-200 rounded-full ">{category}</span>
         </div>
 
@@ -38,7 +59,7 @@ const JobDetails = () => {
       <section className="p-6 w-full  bg-white rounded-md shadow-md flex-1 md:min-h-[350px]">
         <h2 className="text-lg font-semibold text-gray-700 capitalize ">Place A Bid</h2>
 
-        <form>
+        <form onSubmit={handleFormSubmission}>
           <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
             <div>
               <label className="text-gray-700 " htmlFor="price">
@@ -48,7 +69,7 @@ const JobDetails = () => {
                 id="price"
                 type="text"
                 name="price"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-slate-400 focus:ring focus:ring-slate-300 focus:ring-opacity-40 focus:outline-none "
               />
             </div>
 
@@ -62,7 +83,7 @@ const JobDetails = () => {
                 name="email"
                 disabled
                 defaultValue={user?.email}
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-slate-400 focus:ring focus:ring-slate-300 focus:ring-opacity-40  focus:outline-none"
               />
             </div>
 
@@ -74,13 +95,18 @@ const JobDetails = () => {
                 id="comment"
                 name="comment"
                 type="text"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-slate-400 focus:ring focus:ring-slate-300 focus:ring-opacity-40  focus:outline-none"
               />
             </div>
             <div className="flex flex-col gap-2 ">
               <label className="text-gray-700">Deadline</label>
 
-              {/* Date Picker Input Field */}
+              <DatePicker
+                className="border p-2 rounded-md w-full focus:border-slate-400 focus:ring focus:ring-slate-300 focus:ring-opacity-40  focus:outline-none"
+                selected={startDate}
+                dateFormat="dd/MM/yyyy"
+                onChange={(date) => setStartDate(date)}
+              />
             </div>
           </div>
 
